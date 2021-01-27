46eff5b2323479213e70ce194a7c45db78455cbd
LiShanglin <LiShanglin@DESKTOP-0ACGH0P>
2021-01-27 23:35:58 +0800

    添加项目文件。

---
+++ exercise_life_second.cpp
@@ -0,0 +1,15 @@
+﻿#include<stdio.h>
+#define SEC_PER_YEAR
+int main(void)
+{
+	float second, year;
+	printf("Enter how many years old you are:");
+	scanf_s("%f", &year);
+	second = year * SEC_PER_YEAR
+	printf("You are :%.1f year old\n", year);
+	printf("And you %e second old,too.\n", second);
+	
+	getchar();
+	return 0;
+
+}
